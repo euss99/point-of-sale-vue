e2e4f02e0fc11d6750f7100a3e5e8458e623938c
Uriel Solis <euss99@hotmail.com>
2023-08-22 13:18:16 -0600

    Init the project

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import ShopView from '../views/ShopView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: ShopView
+    },
+  ]
+})
+
+export default router
---
+++ src/views/ShopView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Shop View</h1>
+  </div>
+</template>
